db82df6a7e333a87a263cf9668af2e25bfc77eab
Vivek Poddar <vivekimsit@gmail.com>
2018-09-24 20:32:04 +0100

    Add more tests.

--- src/prettybytes.nim
+++ src/prettybytes.nim
@@ -1,14 +1,14 @@
 import math, strformat
 
-const units = ["B", "KB", "MB", "GB", "TB"]
+const units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
 
 proc prettybytes*(number: SomeNumber): string {.noSideEffect.} =
   var
     num = number.float
     suffix = ""
     prefix = ""
-  if (number < 0):
-    num = -number
+  if (num < 0):
+    num = -num
     prefix = "-"
 
   let exponent = int(log10(num) / 3.0) # don't consider number less than n * 1000
--- tests/unit.nim
+++ tests/unit.nim
@@ -1,23 +1,19 @@
-import unittest, prettybytes, strformat
+import math, unittest, prettybytes, strformat
 
-suite "humanize":
+suite "prettybytes":
   test "converts bytes to human readable strings":
-    check prettybytes(10.0) == "10 B"
+    check:
+      prettybytes(0.4) == "0.4 B"
+      prettybytes(10) == "10 B"
+      prettybytes(10.1) == "10.1 B"
+      prettybytes(999.0) == "999 B"
+      prettybytes(1001.0) == "1.001 KB"
+      prettybytes(10.0^16) == "10 PB"
 
-  test "converts bytes to human readable strings":
-    check prettybytes(100.0) == "100 B"
-
-  test "converts bytes to human readable strings":
-    check prettybytes(1000.0) == "1 KB"
-
-  test "converts bytes to human readable strings":
-    check prettybytes(10000.0) == "10 KB"
-
-  test "converts bytes to human readable strings":
-    check prettybytes(1000000.0) == "1 MB"
-
-  test "converts bytes to human readable strings":
-    check prettybytes(1200000.0) == "1.2 MB"
-
-  test "converts bytes to human readable strings":
-    check prettybytes(-1200000.0) == "-1.2 MB"
+  test "supports negative number":
+    check:
+      prettybytes(-0.4) == "-0.4 B"
+      prettybytes(-10.0) == "-10 B"
+      prettybytes(-10.1) == "-10.1 B"
+      prettybytes(-999.0) == "-999 B"
+      prettybytes(-1001.0) == "-1.001 KB"
